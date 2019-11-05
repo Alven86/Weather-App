97352a8b44bbf2be754aa702e9542a5c28dd394b
alven <alvenj@yahoo.com>
2019-11-05 05:13:22 +0100

    Initial Commit

---
+++ Weather-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather-App
+//
+//  Created by Mac OS on 2019-11-05.
+//  Copyright © 2019 Alven. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

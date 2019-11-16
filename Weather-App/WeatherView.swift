5f3ae0fc8494d3d40a73bdd4df1b8ae0a5081f8d
Alven <alvenj@yahoo.com>
2019-11-16 07:28:44 +0100

    User interface and Connections.

---
+++ Weather-App/WeatherView.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherView.swift
+//  Weather-App
+//
+//  Created by Mac OS on 2019-11-16.
+//  Copyright © 2019 Alven. All rights reserved.
+//
+
+import UIKit
+
+class WeatherView: UIView {
+
+   //MARK: IBOutlets
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var cityNameLable: UILabel!
+    @IBOutlet weak var dateLabl: UILabel!
+    @IBOutlet weak var weatherInfoLable: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var bottomContainer: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var hourlyCollectionView: UICollectionView!
+    @IBOutlet weak var infoCollectionView: UICollectionView!
+    
+   
+    
+    
+}
---
+++ Weather-App/WeatherViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherViewController.swift
+//  Weather-App
+//
+//  Created by Mac OS on 2019-11-15.
+//  Copyright © 2019 Alven. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    
+    //MARK: IBOutlets
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    
+    
+    
+    
+    
+    //MARK: ViewLifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}

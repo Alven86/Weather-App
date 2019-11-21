616b8e323b7879433d9867b3df55265e6e681804
Alven <alvenj@yahoo.com>
2019-11-21 22:41:27 +0100

    Create and display info collection cell.

--- Weather-App/Views/WeatherView.swift
+++ Weather-App/Views/WeatherView.swift
@@ -80,8 +80,9 @@ class WeatherView: UIView {
        }
        
        func refreshData() {
-        setupCurrentWeather()
-        //infoCollectionView.reloadData()
+       setupCurrentWeather()
+        setupWearherInfo()
+        infoCollectionView.reloadData()
           
        }
     
@@ -97,6 +98,20 @@ class WeatherView: UIView {
         weatherInfoLabel.text = currentWeather.weatherType
     }
     
+    
+    private func setupWearherInfo() {
+           
+           let windInfo = WeatherInfo(infoText: String(format: "%.1f m/sec", currentWeather.windSpeed), nameText: nil, image: getWeatherIconFor("wind"))
+           let humidityInfo = WeatherInfo(infoText: String(format: "%.0f ", currentWeather.humidity), nameText: nil, image: getWeatherIconFor("humidity"))
+           let pressureInfo = WeatherInfo(infoText: String(format: "%.0f mb", currentWeather.pressure), nameText: nil, image: getWeatherIconFor("pressure"))
+           let visibilityInfo = WeatherInfo(infoText: String(format: "%.0f km", currentWeather.visibility), nameText: nil, image: getWeatherIconFor("visibility"))
+           let feelsLikeInfo = WeatherInfo(infoText: String(format: "%.1f", currentWeather.feelsLike), nameText: nil, image: getWeatherIconFor("feelsLike"))
+           let uvInfo = WeatherInfo(infoText: String(format: "%.1f", currentWeather.uv), nameText: "UV Index", image: nil)
+           let sunriseInfo = WeatherInfo(infoText: currentWeather.sunrise, nameText: nil, image: getWeatherIconFor("sunrise"))
+           let sunsetInfo = WeatherInfo(infoText: currentWeather.sunset, nameText: nil, image: getWeatherIconFor("sunset"))
+           
+           weatherInfoData = [windInfo, humidityInfo, pressureInfo, visibilityInfo, feelsLikeInfo, uvInfo, sunriseInfo, sunsetInfo]
+       }
     
 
         
